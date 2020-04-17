bc8411a6a7dfae3ae686317e81341e20aee835af
mobm65 <60409970+mobm65@users.noreply.github.com>
2020-04-17 12:47:25 +0100

    exploring solutions
    Researching online solutions to analyse this data set.  Still figuring out how to import the data set properly.

--- analysis.py
+++ analysis.py
@@ -2,6 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns 
-import scipy as sp 
+import scipy as sp
+from sklearn import datasets
 
-iris = pd.read_csv("iris.csv")
+#df = pd.read_csv("iris.csv")
+
+iris = datasets.load_iris() 
+
+X = iris.data[:, :2]
+y = iris.target 
+x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+
+plt.figure(2, figsize=(8,6))
+plt.clf() 
+plt.show()
