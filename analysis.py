c810734a5a570ec1ae103f7c32ae445606e3d5ac
mobm65 <60409970+mobm65@users.noreply.github.com>
2020-04-18 13:41:07 +0100

    Experiment with coding
    Improving data set and trying out coding solutions,

--- analysis.py
+++ analysis.py
@@ -5,15 +5,9 @@ import seaborn as sns
 import scipy as sp
 from sklearn import datasets
 
-#df = pd.read_csv("iris.csv")
+df = pd.read_csv("iris.data")
 
-iris = datasets.load_iris() 
-
-X = iris.data[:, :2]
-y = iris.target 
-x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
-y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
-
-plt.figure(2, figsize=(8,6))
-plt.clf() 
+plt.scatter(df['spl'], df['class'])
 plt.show()
+
+
