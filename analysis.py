4fa3918a11fb42fcb9df30b8d0cb11ac95d0462f
mobm65 <60409970+mobm65@users.noreply.github.com>
2020-04-23 12:23:07 +0100

    further research
    Trying other examples to solve the assigned problems.

--- analysis.py
+++ analysis.py
@@ -7,15 +7,15 @@ import scipy as sp
 df = pd.read_csv("iris.data")
 
 a = plt.scatter(df["spl"], df["spw"])
-fig, (ax1, ax2)= plt.subplots(nrows=1, ncols=2, figsize=(0.1,8))
+fig, (ax1)= plt.subplots(nrows=1, ncols=1, figsize=(0.1,8))
 ax1.set_title("Scatter: Variations in Iris Species")
-#ax1.scatter(x=a, y=a, marker="o", c = "r", edgecolor="b")
+ax1.scatter(x=x, y=y, marker="o", c = "r", edgecolor="b")
 ax1.set_xlabel("Sepal length")
 ax1.set_ylabel("Sepal width")
 
 
 b = plt.scatter(df["ptl"], df["ptw"])
-fig, (ax1, ax2)= plt.subplots(nrows=1, ncols=2, figsize=(0.1,8))
+fig, (ax2)= plt.subplots(nrows=1, ncols=1, figsize=(0.1,8))
 #ax1.scatter(x=b, y=b, marker="o", c = "r", edgecolor="b")
 ax1.set_title("Scatter: Variations in Iris Species")
 ax1.set_xlabel("Petal length")
--- analysis1.py
+++ analysis1.py
@@ -4,3 +4,30 @@
 #setosa = open(iris.data, "setosa")
 #versicolor = open(iris.data, "versicolor")
 #virginica = open(iris.data, "virginica"
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns 
+from sklearn import preprocessing
+from Cython.Shadow import inline
+
+
+
+iris = pd.read_csv("iris.data")
+iris.info()
+
+iris['Species'] = iris['Species'].astype('category')
+iris['target'] = pd.Categorical.from_array(iris)['Species'].codes
+iris['target'].value_counts()
+
+ax = iris[iris.Species=='Iris-setosa'].plot.scatter(x='SepalLengthCm', 
+y='SepalWidthCm', color='red', label='setosa')
+iris[iris.Species=='Iris-versicolor'].plot.scatter(x='SepalLengthCm',
+y='SepalWidthCm', color='green', label='versicolor', ax=ax)
+iris[iris.Species=='Iris-virginica'].plot.scatter(x='SepalLengthCm', 
+y='SepalWidthCm', color='blue', label='virginica', ax=ax)
+ax.set_title("Variance in petal & sepal length & width in Iris Species")
+
+
+
+
