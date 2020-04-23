bdb100b4c96b99fa5dcac2a39d8b91731c252c34
mobm65 <60409970+mobm65@users.noreply.github.com>
2020-04-23 20:09:51 +0100

    Perfecting histogram
    Adding information to histograms to plot; petal length, petal width, sepal length and sepal width

--- analysis.py
+++ analysis.py
@@ -1,10 +1,44 @@
+# save a histogram of each variable to a png file
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns 
 import scipy as sp
+from seaborn import colors
+from matplotlib.patches import Patch
 
 iris = pd.read_csv("iris.data")
-iris.info()
+# iris.info()
+# iris['Species'] = iris['Species'].astype('category')
 
-iris['Species'] = iris['Species'].astype('category')
+plt.hist(iris['ptl'], facecolor='purple', edgecolor='black')
+plt.xlabel('Petal length in cms')
+plt.ylabel('Frequency')
+plt.title('Frequency of Petal length in three species of Iris')
+plt.show()
+
+
+plt.hist(iris['ptw'], facecolor='green', edgecolor='black')
+plt.xlabel('Petal width in cms')
+plt.ylabel('Frequency')
+plt.title('Frequency of Petal width in three species of Iris')
+plt.show()
+
+plt.hist(iris['spl'], facecolor='orange', edgecolor='black')
+plt.xlabel('Sepal length in cms')
+plt.ylabel('Frequency')
+plt.title('Frequency of Sepal length in three species of Iris')
+plt.show()
+
+plt.hist(iris['spw'], facecolor='yellow', edgecolor='black')
+plt.xlabel('Sepal width in cms')
+plt.ylabel('Frequency')
+plt.title('Frequency of Sepal width in three species of Iris')
+plt.show()
+
+#plt.plot()
+#ax = iris[iris.Species=='Iris-setosa'].plot.trend(x='spl', 
+#y='spw', color='red', label='setosa')
+
+
+#plt.show()
--- analysis2.py
+++ analysis2.py
@@ -1,1 +1,11 @@
-# create a histogram and save it to a png file
+# a summary of each variable to a text file
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.pyplot import show
+from numpy.ma import sort
+from pandas import arrays
+
+iris = pd.read_csv("iris.data")
+arrays('ptl')
+#print(iris)
