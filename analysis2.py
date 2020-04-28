78108477b2ceb52106703558786db0819c0a3583
mobm65 <60409970+mobm65@users.noreply.github.com>
2020-04-28 11:17:44 +0100

    Output from code to terminal
    I cannot get this code to output to a text file, I am being denied permission.  I may have made an error when setting up studio visual code or perhaps my antivirus is the problem.  I am not able to fix the issue so must submit this part of the assignment in this format.

--- analysis2.py
+++ analysis2.py
@@ -2,11 +2,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import seaborn as sns
-from seaborn import colors
 
+# import the iris dataset
 iris = pd.read_csv("iris.data")
+# separate the iris variables for analysis
 data = pd.DataFrame(iris, columns=['spl','spw','ptl', 'ptw', 'Species'])
+# extract the statistical summary of each variable and print to screen
+print(data.describe())
 
-sns.pairplot(data, hue="Species")
-plt.show()
+# this codes analyses the data and gives a summary of the
+# statistical analysis to a text file
+# I have commented the code because it would not give me 
+# permission to write to a text file.
+
+
+#text_file = open("analysis.txt", "w")
+#n = text_file.write(data.describe())
+#text_file.close()
+
+
